1551476e463b61c8f094df9ac8e6c7ecf8db7379
kevcodeus <kevinpthankachen@gmail.com>
2026-01-08 19:17:14 +0530

    c

--- app.py
+++ app.py
@@ -156,4 +156,4 @@ def level_hard():
                                                                box_class="safe-box"))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=6000)
